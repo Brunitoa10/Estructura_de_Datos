59d7c8b7b124d0cffb43ae39cbda1445a0b8bd6a
Bruno Ariel Parisi <bnoprs00@gmail.com>
2022-12-21 17:52:04 -0300

    Primer commit- Solo es una prueba

---
+++ Estructura_de_Datos_Version_Final/TDA_Pila/Excepciones/EmptyStackException.java
@@ -0,0 +1,12 @@
+package Excepciones;
+
+public class EmptyStackException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmptyStackException(String s) {
+		super(s);
+	}
+}
