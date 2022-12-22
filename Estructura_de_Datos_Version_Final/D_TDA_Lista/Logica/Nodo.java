9de847c5c77f0ddfe4231cae6d8b37c1425cebc8
Bruno Ariel Parisi <bnoprs00@gmail.com>
2022-12-22 00:15:59 -0300

    13 - Implementacion Nodo en TDA_Lista

---
+++ Estructura_de_Datos_Version_Final/D_TDA_Lista/Logica/Nodo.java
@@ -0,0 +1,42 @@
+package Logica;
+
+import Interfaces.Position;
+
+public class Nodo<E> implements Position<E> {
+	
+	private E elemento;
+	private Nodo<E> siguiente;
+	
+	public Nodo(E item, Nodo<E> sig) {
+		elemento= item;
+		siguiente = sig;
+	}
+	
+	public Nodo(E item) {
+		this(item,null);
+	}
+	
+	//SETTERS
+	public void setElemento(E elemento) {
+		this.elemento = elemento;
+	}
+	
+	public void setSiguiente(Nodo<E> siguiente) {
+		this.siguiente = siguiente;
+	}
+	
+	//GETTERS
+	public E getElemento() {
+		return elemento;
+	}
+	
+	public Nodo<E> getSiguiente(){
+		return siguiente;
+	}
+	
+	@Override
+	public E element() {
+		// TODO Auto-generated method stub
+		return elemento;
+	}
+}
--- Estructura_de_Datos_Version_Final/D_TDA_Lista/Logica/listaSimplementeEnlazada.java
+++ Estructura_de_Datos_Version_Final/D_TDA_Lista/Logica/listaSimplementeEnlazada.java
@@ -19,61 +19,79 @@ public class listaSimplementeEnlazada<E> implements PositionList<E>{
 		head = null;
 		size = 0;
 	}
-	
+
 	@Override
 	public int size() {
+		// TODO Auto-generated method stub
 		return size;
 	}
-	
+
 	@Override
 	public boolean isEmpty() {
+		// TODO Auto-generated method stub
 		return size == 0;
 	}
-	
+
 	@Override
 	public Position<E> first() throws EmptyListException {
+		if(isEmpty()) {
+			throw new EmptyListException ("Error: Lista vacia.")
+		}
 		return head;
 	}
-	
+
 	@Override
 	public Position<E> last() throws EmptyListException {
+		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
-	public Position<E> prev(Position<E> p) throws BoundaryViolationException, InvalidPositionException {
+	public Position<E> next(Position<E> p) throws InvalidPositionException, BoundaryViolationException {
+		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
-	public Position<E> next(Position<E> p) throws BoundaryViolationException, InvalidPositionException {
+	public Position<E> prev(Position<E> p) throws InvalidPositionException, BoundaryViolationException {
+		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
-	public E set(Position<E> p, E e) throws InvalidPositionException {
-		return null;
+	public void addFirst(E element) {
+		// TODO Auto-generated method stub
+		
 	}
-	
+
+	@Override
+	public void addLast(E element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addAfter(Position<E> p, E element) throws InvalidPositionException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addBefore(Position<E> p, E element) throws InvalidPositionException {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
 	public E remove(Position<E> p) throws InvalidPositionException {
+		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
-	public void addFirst(E e) {
-	}
-	
-	@Override
-	public void addLast(E e) {
-	}
-	
-	@Override
-	public void addBefore(Position<E> p, E e) throws InvalidPositionException {
-	}
-	
-	@Override
-	public void addAfter(Position<E> p) throws InvalidPositionException {
+	public E set(Position<E> p, E element) throws InvalidPositionException, BoundaryViolationException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
@@ -87,4 +105,6 @@ public class listaSimplementeEnlazada<E> implements PositionList<E>{
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
 }
