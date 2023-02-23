3a0c5386345425dc591610cd17f5135fb96a78c3
Bruno Ariel Parisi <bnoprs00@gmail.com>
2023-02-23 11:01:18 -0300

    Comentarios

--- Estructura_de_Datos_Version_Final/src/O_TDA_Grafo_No_Dirigido/GrafoNoDirigidoListaDeAdyacencia.java
+++ Estructura_de_Datos_Version_Final/src/O_TDA_Grafo_No_Dirigido/GrafoNoDirigidoListaDeAdyacencia.java
@@ -232,24 +232,28 @@ public class GrafoNoDirigidoListaDeAdyacencia<V, E> implements GraphNoDirigido<V
 	//Ejercicios
 	public PositionList<Vertex<V>> caminoEconomico(GraphNoDirigido<V, E> g, Vertex<V> origen, Vertex<V> destino) {
 
-		// Almacenamos un mapeo de los vertices visitados
+		// Creo un mapeo de los vertices donde almaceno si fueron visitados o no
 		// <VerticeO,false> --> el vertice O no fue visitado
 		// <VerticeO,true> --> el vertice O  fue visitado
 		
 		Map<Vertex<V>,Boolean> visitados = new MapeoConListaDoblementeEnlazada<Vertex<V>,Boolean>();
 		
 		//Almaceno el costo de recorreer un camino y su costo tiene que ser el menor posible
-		
 		Par p = new Par();
 		p.setPeso(Integer.MAX_VALUE);
-		PositionList<Vertex<V>> L = new listaDoblementeEnlazada<Vertex<V>>();
+		
+		//Creo una lista donde voy a almacenar los vertices que conforman el camino mas economico
+		PositionList<Vertex<V>> list = new listaDoblementeEnlazada<Vertex<V>>();
 		int pesoAux = 0;
 		
+		
 		try {
+			//Para cada vertice v del grafo g marcos el vertice v como no visitado
 			for(Vertex<V> v : g.vertices()) {
 				visitados.put(v, false);
 			}
-			caminoEcoAux(origen,destino,g,visitados,p,L,pesoAux);
+			//Llamo al metodo recursivo
+			caminoEcoAux(origen,destino,g,visitados,p,list,pesoAux);
 		} catch (InvalidKeyException e) {
 			System.out.println(e.getMessage());
 		}
@@ -257,24 +261,34 @@ public class GrafoNoDirigidoListaDeAdyacencia<V, E> implements GraphNoDirigido<V
 	}
 	
 	private Par caminoEcoAux(Vertex<V> origen, Vertex<V> destino, GraphNoDirigido<V, E> g,Map<Vertex<V>, Boolean> visitados, Par p, PositionList<Vertex<V>> caminoActual, int pesoAux) {
-		
 		try {
+			//Agrego al final de la lista el vertice origen
 			caminoActual.addLast(origen);
+			
+			//Marco al vertice origen como visitado
 			visitados.put(origen,true);
-			if(origen.equals(destino)) {
-				if(pesoAux < p.getPeso()) {
+			
+			//Si solo hay un vertice y el peso <= al peso del camino actual
+			if(origen == destino) {
+				if(pesoAux <= p.getPeso()) {
+					//Hago una clocacion del camino actual y actualizo su peso
 					p.setCamino_minimo(clonar(caminoActual));
 					p.setPeso(pesoAux);
 				}
 			}else {
-				for(Edge<E> arco : g.incidentEdges(origen)) {
-					Vertex<V> opuesto = g.opposite(origen, arco);
+				//Para cada arco arc de los arcos incidentes del origen en g
+				for(Edge<E> arc : g.incidentEdges(origen)) {
+					//Obtener el vertice opuesto del vertice origen y el arco arc
+					Vertex<V> opuesto = g.opposite(origen, arc);
+					//Si el vertice opuesto no esta visitado
 					if(visitados.get(opuesto) == false) {
-						int	pesoArco = pesoAux +(int) arco.element();
-						caminoEcoAux(opuesto,destino,g,visitados,p,caminoActual,pesoAux+pesoArco);
+						//Actualizo el peso del arco y llamo al metodo recursivo
+						pesoAux +=(int) arc.element();
+						caminoEcoAux(opuesto,destino,g,visitados,p,caminoActual,pesoAux);
 					}
 				}
 			}
+			//Elimino el ultimo vertice de la lista y marco al vertice origen como no visitado
 			caminoActual.remove(caminoActual.last());
 			visitados.put(origen, false);
 		} catch (InvalidKeyException | InvalidVertexException | InvalidEdgeException | InvalidPositionException | EmptyListException e) {
@@ -282,6 +296,7 @@ public class GrafoNoDirigidoListaDeAdyacencia<V, E> implements GraphNoDirigido<V
 		}
 		return p;
 	}
+	
 	private PositionList<Vertex<V>> clonar(PositionList<Vertex<V>> camino_actual) {
 		PositionList<Vertex<V>> clon = new listaDoblementeEnlazada<Vertex<V>>();
 		PositionList<Vertex<V>> aux = new listaDoblementeEnlazada<Vertex<V>>();
@@ -305,4 +320,5 @@ public class GrafoNoDirigidoListaDeAdyacencia<V, E> implements GraphNoDirigido<V
 
 		return clon;
 	}
+	
 }
